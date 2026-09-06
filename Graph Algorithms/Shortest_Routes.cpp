c933d1975ba98da641c34ce3a70e5175964b6113
apper-o <kulkacper@gmail.com>
2026-09-06 13:58:04 +0200

    Reupload of a solution

--- Graph Algorithms/Shortest_Routes.cpp
+++ Graph Algorithms/Shortest_Routes.cpp
@@ -1,60 +1,60 @@
 #include <bits/stdc++.h>
-#define pb push_back
-#define ll long long 
 using namespace std;
-//------------------------code------------------------//
-const int MAXN = 1e5+3;
-const ll INF = 1e18+3;
-const int X = 319;
- 
-struct pii{
-    int a; ll b;
+
+const long long INF = 1e18;
+
+struct Edge {
+    int to;
+    long long cost;
 };
- 
-int n, m;
-vector<pii> adj[MAXN];
-vector<ll> dist(MAXN, INF);
- 
-struct cmp{ // a is above b if operator() = false
-    bool operator() (pii a, pii b){
-        return a.b > b.b;
-    }
-};
- 
-priority_queue<pii, vector<pii>, cmp> pq;
- 
-void solve()
-{
-    pq.push({1, 0});
-    dist[1]=0;
-    while(pq.size())
-    {
-        auto [v, c] = pq.top();
+
+void dijkstra(int start, const vector<vector<Edge>>& adj, vector<long long>& dist) {
+    priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+
+    dist[start] = 0;
+    pq.push({0, start});
+
+    while (!pq.empty()) {
+        auto [d, v] = pq.top();
         pq.pop();
-        if(c>dist[v])  
+
+        if (d > dist[v])
             continue;
-        for(auto [u, w] : adj[v])
-        {
-            if(dist[u] > c + w)
-            {   
-                dist[u] = c + w;
-                pq.push({u, c+w});
+
+        for (const auto& edge : adj[v]) {
+            if (dist[edge.to] > d + edge.cost) {
+                dist[edge.to] = d + edge.cost;
+                pq.push({dist[edge.to], edge.to});
             }
         }
     }
-    for(int i=1;i<=n;i++)
-        cout<<dist[i]<<" ";
 }
- 
-int main()
-{
-    cin.tie(0); ios_base::sync_with_stdio(0);
-    cin>>n>>m;
-    for(int i=0,a,b,c;i<m;i++)
-    {
-        cin>>a>>b>>c;
-        adj[a].pb({b, c});
-    }  
-    solve();
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<Edge>> adj(n);
+
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        long long c;
+        cin >> a >> b >> c;
+        a--;
+        b--;
+        adj[a].push_back({b, c});
+    }
+
+    vector<long long> dist(n, INF);
+    dijkstra(0, adj, dist);
+
+    for (int i = 0; i < n; i++) {
+        cout << dist[i] << (i + 1 == n ? "" : " ");
+    }
+    cout << "\n";
+
     return 0;
 }
